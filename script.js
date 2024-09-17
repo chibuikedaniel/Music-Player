e4c07f48f55cda08847f3a92b40e5760621167be
dracuzi <danielchibuike77@gmail.com>
2024-09-17 23:32:58 +0100

    added pause functionality

--- script.js
+++ script.js
@@ -94,6 +94,12 @@ const playSong = (id) => {
     audio.play();
 }
 
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+    playButton.classList.remove('playing');
+    audio.pause();
+};
+
 const renderSongs = (array) => {
     const songsHTML = array.map((song) => {
         return `<li id="song-${song.id}" class="playlist-song">
