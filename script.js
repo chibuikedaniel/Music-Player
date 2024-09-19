949cc743e1b978bca42d7faac96c9a66b0508380
dracuzi <danielchibuike77@gmail.com>
2024-09-19 21:41:43 +0100

    added a function to play orevious songs

--- script.js
+++ script.js
@@ -108,7 +108,16 @@ const playNextSong = () => {
         const nextSong = userData?.songs[currentSongIndex + 1];
         playSong(nextSong.id)
     }
-}
+};
+
+const playPreviousSong = () => {
+    if (userData?.currentSong === null) return;
+    else {
+        const currentSongIndex = getCurrentSongIndex();
+        const previousSong = userData?.songs[currentSongIndex - 1];
+        playSong(previousSong.id);
+    }
+};
 
 const renderSongs = (array) => {
     const songsHTML = array.map((song) => {
