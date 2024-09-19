23e9b3b5cab475f5aa221d843c2c6076fa8ae207
dracuzi <danielchibuike77@gmail.com>
2024-09-19 22:10:33 +0100

    added a function to highlight current playing song

--- script.js
+++ script.js
@@ -119,6 +119,18 @@ const playPreviousSong = () => {
     }
 };
 
+const highlightCurrentSong = () => {
+    const playlistSongElements = document.querySelectorAll(".playlist-song");
+    const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`
+    );
+    playlistSongElements.forEach((songEl) => {
+        songEl.removeAttribute("aria-current");
+    });
+    if (songToHighlight) {
+        songToHighlight.setAttribute("aria-current", "true");
+    };
+}
+
 const renderSongs = (array) => {
     const songsHTML = array.map((song) => {
         return `<li id="song-${song.id}" class="playlist-song">
