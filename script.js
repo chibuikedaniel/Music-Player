cda3322ca844cb54511b1a2cb4ad77697270b43a
dracuzi <danielchibuike77@gmail.com>
2024-09-19 23:38:54 +0100

    added a function to reset the playlist if array === 0

--- script.js
+++ script.js
@@ -147,6 +147,16 @@ const deleteSong = (id) => {
     renderSongs(userData?.songs)
     highlightCurrentSong()
     setPlayButtonAccessibleText()
+
+    if (userData?.songs.length === 0) {
+        const resetButton = document.createElement("button");
+        const resetText = document.createTextNode("Reset Playlist");
+
+        resetButton.id = "reset";
+        resetButton.ariaLabel = "Reset playlist";
+        resetButton.appendChild(resetText);
+        playlistSongs.appendChild(resetButton);
+    }
 }
 
 const setPlayerDisplay = () => {
